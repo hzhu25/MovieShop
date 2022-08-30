3eeb1333f6152d14f32967a6dfb9c0965e85b8c7
Hong Zhu <hzhu25@syr.edu>
2022-08-30 00:48:15 -0700

    finished Authentication and Authorization

--- MovieShopMVC/Controllers/AccountController.cs
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
 
 namespace MovieShopMVC.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginModel model)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterModel model, string FIRSTNAME, string firstname, string xyz)
         {
             return View();
         }
---
+++ MovieShopMVC/Models/UserLoginModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MovieShopMVC.Models
+{
+    public class UserLoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+
